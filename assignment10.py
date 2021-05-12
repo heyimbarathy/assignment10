361ab8d5a675970ca0debc9ee706fbb4375a2454
Bharathy <heyimbarathy@gmail.com>
2021-05-12 10:42:02 +0530

    assignment10.py fim=nished with turtle library

--- assignment10.py
+++ assignment10.py
@@ -1,1 +1,15 @@
-print("hello")
+# importing turtle library
+import turtle
+
+val = 50
+pen = turtle.Turtle()
+pen.color("orange","green")
+pen.pensize(2)
+pen.shape("turtle")
+
+for i in range(val):
+    pen.forward(i*10)
+    # in clockwise
+    pen.right(144)
+
+turtle.done()
